9debd5c21c537efde41727edfaeee4d7ea81cc1f
Jonathan Perez <jpmono416@gmail.com>
2021-05-31 00:05:18 +0100

    Previous commit didn't include FileUtilities.cs by mistake. Changes on previous commit refer to this file.

--- CopyDirectory/FileUtilities.cs
+++ CopyDirectory/FileUtilities.cs
@@ -26,77 +26,88 @@ namespace CopyDirectory
         }
 
         /**
-         * Copy a directory and all of its contents to a new given location.
+         * This function moves the source directory (including the directory)
+         * onto the destination location. Resulting in destination/sourceDir/*
+         * The Move function implements the same behaviour.
+         * 
          * <param name="dirSourcePath">The source (original) path of the directory</param>
          * <param name="dirDestinationPath"></param>
          * <param name="overwrite"></param>
-         * <param name="move"></param>
+         * <param name="mergeDirectories">Whether to blend the source directory into the destination in case it is preexisting, or cancel the operation.</param>
          */
-        public static string CopyDirectory(string dirSourcePath, string dirDestinationPath, bool overwrite = false)
+        public static string CopyDirectory(string dirSourcePath, string dirDestinationPath, bool overwrite = false, bool mergeDirectories = false)
         {
             // Check no data is null
             var paths = new[] {dirSourcePath, dirDestinationPath};
             if(!CheckNotNull(paths)) return GetMessage(ReturnMessages.NullData);
             
-            // Check dir exists
-            if (!DirectoryExists(dirSourcePath)) return GetMessage(ReturnMessages.DirDoesNotExist);
-
-            // Get all files
-            var filePaths = Directory.GetFiles(dirSourcePath);
+            var finalDirPath = Path.Combine(dirDestinationPath, new DirectoryInfo(dirSourcePath).Name);
             
-            /*
-             * Copy function only throws when data is not supplied or it's empty but
-             * we already checked for null, so we don't need to catch ArgumentNullException
-             */
+            if (Directory.Exists(finalDirPath) && !mergeDirectories)
+                return GetMessage(ReturnMessages.InvalidMoveLocation);
+            
+            // Check dir exists
+            if (!Directory.Exists(dirSourcePath)) return GetMessage(ReturnMessages.DirDoesNotExist);
+            if (!Directory.Exists(finalDirPath)) Directory.CreateDirectory(finalDirPath);
+            
             try
             {
-                foreach (var file in filePaths)
+                // Copy all files
+                foreach (var file in Directory.GetFiles(dirSourcePath))
                 {
-                    // Get file name from current path to build final path 
-                    var name = Path.GetFileName(file);
-                    var destinationFullAddress = Path.Combine(dirDestinationPath, name);
+                    // Get file name from current path to build final path
+                    var finalFilePath = Path.Combine(finalDirPath, Path.GetFileName(file));
                 
                     // Execute copy function
-                    CopyFile(file, destinationFullAddress, overwrite);
+                    CopyFile(file, finalFilePath, overwrite);
                     
-                    /* One issue with this approach is that if IOException occurs, the copied elements remain copied. 
+                    /*
+                     * One issue with this approach is that if IOException occurs, the copied elements remain copied. 
                      * An approach to fx this is goes something as
-                     * create temporary folder
-                     * copy each file onto temp folder after checking it does not exist into the actual folder
-                     * destroy temp folder if error occurs. In this way either all or nothing is copied.
-                     * TODO Another approach is to avoid copying duplicated files by catching in Copy and skipping duplicated
+                     *      create temporary folder
+                     *      copy each file onto temp folder after checking it does not exist into the actual folder
+                     *      destroy temp folder if error occurs. In this way either all or nothing is copied.
+                     * Another approach is to avoid copying duplicated files by try/catching in Copy funct and skip duplicates
                      */
                 }
-
-                return GetMessage(ReturnMessages.CopiedSuccessfully);
+                
+                //Copy all the directories by invoking itself with each nested dir's path
+                foreach (var dir in Directory.GetDirectories(dirSourcePath, "*", SearchOption.TopDirectoryOnly))
+                    CopyDirectory(dir, finalDirPath, overwrite, mergeDirectories);
             }
-            // No need to throw error and stop program execution, just return error message
-            // IOException means 1 particular error in this context (internal code 80, file already exists)
+            
+            // IOException can be only 1 particular error in this context (internal code 80, file already exists)
             // ArgumentException and ArgumentNullException cannot be catched, as the code would not reach the try statement
-            catch (IOException ioExcp) { return GetMessage(ReturnMessages.OverwriteError); } 
-            catch (Exception e) { return GetMessage(ReturnMessages.FailedToCopy); }
-        }
-
-        public static string MoveDirectory(string dirSourcePath, string dirDestinationPath, bool overwrite = false)
-        {
-            // Copy contents over
-            var output = CopyDirectory(dirSourcePath, dirDestinationPath, overwrite);
+            catch (IOException) { return GetMessage(ReturnMessages.OverwriteError); } 
+            catch (Exception) { return GetMessage(ReturnMessages.FailedToCopy); }
             
-            // Remove contents after successful copy or inform user of otherwise
-            if (output != GetMessage(ReturnMessages.CopiedSuccessfully)) return output;
-            
-            try { Directory.Delete(dirSourcePath, true); }
-            catch (IOException e) { Console.WriteLine(e.Message); }
-
-            // Return OK message
-            return GetMessage(ReturnMessages.MovedSuccessfully);
+            return GetMessage(ReturnMessages.CopiedSuccessfully);
         }
 
         /**
-         * Return whether a given directory exists or not. Using function to improve
-         * code readability and ease of usage.
+         * This function moves the source directory (including the directory)
+         * onto the destination location. Resulting in destination/sourceDir/*
+         * The Move function implements the same behaviour.
+         * <param name="mergeDirectories">Whether to blend the source directory into the destination in case it is preexisting, or cancel the operation.</param>
          */
-        private static bool DirectoryExists(string path) => Directory.Exists(path);
+        public static string MoveDirectory(string dirSourcePath, string dirDestinationPath, bool overwrite = false, bool mergeDirectories = false)
+        {
+            // Check no data is null - this is executed twice as Copy does it too,
+            // however, it is an important-enough check to have at the beginning of both these functions.
+            // Possible improvement for this solution if I worked more time on it -> Avoid duplicating these 2 lines
+            var paths = new[] {dirSourcePath, dirDestinationPath};
+            if(!CheckNotNull(paths)) return GetMessage(ReturnMessages.NullData);
+
+            // Stop if there's an error in the copy process
+            var output = CopyDirectory(dirSourcePath, dirDestinationPath, overwrite);
+            if (output != GetMessage(ReturnMessages.CopiedSuccessfully)) return output;
+            
+            // Delete function uses the recycling bin
+            try { Directory.Delete(dirSourcePath, true); }
+            catch (IOException e) { Console.WriteLine(e.Message); }
+            
+            return GetMessage(ReturnMessages.MovedSuccessfully);
+        }
 
         /**
          * This function checks that a series of items are not null.
@@ -120,6 +131,7 @@ namespace CopyDirectory
                 case ReturnMessages.DirDoesNotExist     : return "Directory does not exist. Please ensure path is correct.";
                 case ReturnMessages.FileDoesNotExist    : return "File does not exist. Please ensure path is correct.";
                 case ReturnMessages.NullData            : return "Please input all data required.";
+                case ReturnMessages.InvalidMoveLocation : return "Merge directories option is set to false and there is already a directory at the destination location.";
                 case ReturnMessages.OverwriteError      : return "Input/Output error. Copy was invoked with param 'overwrite' set to" +
                                                                  " false and at least one file already exists. Please set to true and try again.";
             }
@@ -137,6 +149,7 @@ namespace CopyDirectory
         FailedToCopy,
         CopiedSuccessfully,
         MovedSuccessfully,
+        InvalidMoveLocation,
         OverwriteError,
         NullData
     }
