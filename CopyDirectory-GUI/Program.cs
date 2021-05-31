d84dddc73d43b417062bcd23005fc51195103fb5
Jonathan Perez <jpmono416@gmail.com>
2021-05-31 04:28:17 +0100

    Added some files that were missing.

---
+++ CopyDirectory-GUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CopyDirectory_GUI
+{
+    /**
+     * This is the app's GUI. It builds and runs commands based on the user input.
+     */
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // This path assumes that both final executable files will be in the same directory
+            var exePath = "CopyDirectory.exe";
+            Process.Start(exePath, @"copy C:\Users\jpmon\Desktop\in C:\Users\jpmon\Desktop\out");
+
+            Console.ReadLine();
+
+        }
+    }
+}
